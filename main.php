6b91e1abc959b4e9720308ec892181440c277eda
Vincent Hendriks <vincent.hendriks@paqt.com>
2023-03-28 17:14:21 +0200

    initial commit

--- main.php
+++ main.php
@@ -6,6 +6,12 @@ function dd(...$args) {
     var_dump($args); die;
 }
 
+function getSearchTerms(): array
+{
+    global $argv;
+    return explode(' ', $argv[1]);
+}
+
 function printTable(array $data) {
     printf("| %40s | %20s | %10s |\n", "Document", "Term", "TDIDF");
     foreach($data as $entry) {
@@ -23,12 +29,10 @@ foreach($documents as $document) {
     $registry->register($document->getName(), $map);
 }
 
-printTable($registry->calculateTFIDF());
-
-
-
-
-
+/**
+ * TODO: improve upon this.
+ */
+$ratings = $registry->calculateTFIDF();
 
 /**
  * Searching a term:
@@ -38,3 +42,29 @@ printTable($registry->calculateTFIDF());
  * 4. Order the results by score descending.
  * 5. Return a list of X best results.
  */
+$documentRatings = [];
+foreach (getSearchTerms() as $term) {
+    foreach($ratings as $rating) {
+        [$document, $word, $score] = $rating;
+        // Second is the word.
+        if ($word == $term) {
+            // Already matched the document on some other term
+            if (array_key_exists($document, $documentRatings)) {
+                $tmp = &$documentRatings[$document];
+                $tmp['matching words'][] = $term;
+                $tmp['relevance'] += $score;
+            } else {
+                $documentRatings[$document] = [
+                    'document'       => $document,
+                    'matching words' => [$term],
+                    'relevance'      => $score
+                ];
+            }
+        }
+    }
+}
+
+uasort($documentRatings, function ($first, $second) {
+    return $first['relevance'] > $second['relevance'] ? 1 : -1;
+});
+print_r($documentRatings);
